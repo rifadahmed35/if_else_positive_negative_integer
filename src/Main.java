8f89e787e92d5bc361e16eebd05cc025eb67e353
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-05 00:06:45 +0100

    Finding positive and negative integer

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=10;
+        if(i>=0){
+            System.out.println("The number is posotive.");
+        } else {
+            System.out.println("The number is negative");
+        }
+
+    }
+}
